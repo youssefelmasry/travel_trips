47ca2b72742a7c9305eddc111775abf7cb675269
yaQot <mohamedyakut0@gmail.com>
2020-11-23 22:46:42 +0200

    dffdfdf

---
+++ hello-react/src/Component/Nav/navitems.js
@@ -0,0 +1,16 @@
+export const menuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-link"
+    },    {
+        title : "Shop",
+        url : "/shop",
+        cName : "nav-link"
+    },    {
+        title : "Contact",
+        url : "/contact",
+        cName : "nav-link"
+    }
+
+]
