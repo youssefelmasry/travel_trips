c9bd0dd302fe29006cf39ff9dcb1d527b0038a4b
Mohamed Ayman <moh.ayman.3600@gmail.com>
2021-01-18 19:15:41 +0200

    increased  ACCESS_TOKEN_LIFETIME

--- backend/travel/travel/settings.py
+++ backend/travel/travel/settings.py
@@ -87,7 +87,7 @@ SIMPLE_JWT_SIGNING_KEY = os.environ.get('SIMPLE_JWT_SIGNING_KEY')
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
