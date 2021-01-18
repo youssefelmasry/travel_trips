23e6f85d28519ebf5fb67272b2b6009f8c2a32cd
Mohamed Ayman <moh.ayman.3600@gmail.com>
2021-01-18 19:22:24 +0200

    Added  LoginSerializer and PasswordChangeSerializer

--- backend/travel/users/serializers.py
+++ backend/travel/users/serializers.py
@@ -36,6 +36,26 @@ class SignUpSerializerWithToken(serializers.ModelSerializer):
         fields = ['token', 'email', 'password', ]
 
 
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=300, required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError(
+                'Current password does not match')
+        return value
+
+    def validate_new_password(self, value):
+        password_validation.validate_password(value)
+        return value
+
+
 class SocialSerializer(serializers.Serializer):
     """Serializer which accepts an OAuth2 access token."""
     access_token = serializers.CharField(
