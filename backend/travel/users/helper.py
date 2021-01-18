8291adfc6ed6413d7a70741c4f89a3abb090efc3
Mohamed Ayman <moh.ayman.3600@gmail.com>
2021-01-18 19:19:42 +0200

    Added get_upload_path and get_upload_path helper functions

--- backend/travel/users/helper.py
+++ backend/travel/users/helper.py
@@ -1,4 +1,6 @@
 """Module for helper functions"""
+from django.contrib.auth import authenticate
+from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
@@ -9,3 +11,22 @@ def get_tokens_for_user(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+
+
+def get_upload_path(instance, filename):
+    """
+    Creates image upload path dynamicly, depending on the uploading model.
+    """
+    model = instance.__class__
+    name = model.Meta.verbose_name_plural.replace(' ', '_')
+    filename = str(instance)
+    return f'static/{name}/images/{filename}'
+
+
+def get_and_authenticate_user(email, password):
+    """Authenticate user in during login and return instance of it."""
+    user = authenticate(username=email, password=password)
+    if user is None:
+        raise serializers.ValidationError(
+            "Invalid username/password. Please try again!")
+    return user
